5642cc506ee3584688a28519872cb74746856b49
_Kairo_ <kairo.h.seidler@gmail.com>
2024-08-21 18:02:09 -0300

    Teste Teste Teste

--- AtividadePedro2.java
+++ AtividadePedro2.java
@@ -1,5 +1,11 @@
 public class AtividadePedro2   {
     public static void main(String[] args) {
 
+
+
+        System.out.println("teste teste");
+        for (int i = 0; i < 8 ; i++) {
+            System.out.println(i);
+        }
     }
 }
