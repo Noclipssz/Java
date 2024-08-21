81410f1c1de98f43b1fa54277adf49bbb7907cd4
_Kairo_ <kairo.h.seidler@gmail.com>
2024-08-21 18:29:12 -0300

    inserindo arquivos que talvez são importantes

--- AtividadePedro2.java
+++ AtividadePedro2.java
@@ -2,22 +2,5 @@ public class AtividadePedro2   {
     public static void main(String[] args) {
 
 
-
-        System.out.println("teste teste");
-        for (int i = 0; i < 8 ; i++) {
-            System.out.println(i);
-        }
-        for (int i = 0; i < 8 ; i++) {
-            System.out.println(i);
-        }
-        for (int i = 0; i < 8 ; i++) {
-            System.out.println(i);
-            System.out.println(i);
-        }
-        for (int i = 0; i < 8 ; i++) {
-            System.out.println(i);
-        }
     }
-
-
 }
