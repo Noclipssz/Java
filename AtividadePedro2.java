023ec870d0c2ea635212a2262cde7480b42e9081
_Kairo_ <kairo.h.seidler@gmail.com>
2024-08-21 18:12:27 -0300

    Mais um teste

--- AtividadePedro2.java
+++ AtividadePedro2.java
@@ -10,5 +10,11 @@ public class AtividadePedro2   {
         for (int i = 0; i < 8 ; i++) {
             System.out.println(i);
         }
+        for (int i = 0; i < 8 ; i++) {
+            System.out.println(i);
+            System.out.println(i);
+        }
+
     }
+
 }
