225402b396be53d2edd43ec39396a1ea616e2a4a
_Kairo_ <kairo.h.seidler@gmail.com>
2024-08-26 19:38:00 -0300

    update

--- AtividadePedro11.java
+++ AtividadePedro11.java
@@ -1,30 +1,9 @@
 import Package.ClassBanc;
 public class AtividadePedro11 {
     public static void main(String[] args) {
+
         ClassBanc banco = new ClassBanc();
         banco.MainAplication();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
